b1ee187accddf605d80201f5b4079b96dea982b1
abdelmonem88 <abdelmonem1888@gmail.com>
2021-03-29 16:02:29 +0200

    this is my first comment

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+ //owl carousel
+ //  $(".owl-carousel").owlCarousel({
+ //   loop: true,
+ //   margin: 0,
+ //   nav: true,
+ //   dots: false,
+ //   responsive: {
+ //    0: {
+ //     items: 1,
+ //    },
+ //    600: {
+ //     items: 3,
+ //    },
+ //    1000: {
+ //     items: 1,
+ //    },
+ //   },
+ //  });
+});
